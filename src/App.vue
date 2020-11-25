5551572cbce9f301330613ee7fbb4e943d11ed17
irfan maulanasam <irfanmaulanasam@irfans-MacBook-2.local>
2020-11-25 13:43:55 +0700

    initiating layout

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div id="main">
+    <div id="sidebar">sidebar</div>
+    <div id="playlist">playlist</div>
+    <div id="ads-space">ads-space</div>
+    </div>
+    <div id="player">
+      <player/>
+    </div>
+  </div>
+</template>
+
+<script>
+import player from '@/components/player'
+export default {
+  components:{
+    player
+  }
+}
+</script>
+
+<style>
+html, body{
+  margin: 0%;
+  height: 100%;
+  overflow: hidden;
+}
+#main{
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  background: whitesmoke;
+  height: 95vh;
+}
+#sidebar{
+  float: left;
+  width: 25%;
+  border:thin 1px green
+}
+#ads-space{
+  float: right;
+  width: 20%;
+  border: solid 2px red;
+}
+#playlist{
+  width: 80%;
+  border:solid 2px springgreen;
+}
+#player{
+  position: fixed;
+  z-index: 20;
+  background: #000;
+  left: 0;
+  bottom: 0.1em;
+  width:100%;
+  height: 5em;
+}
+</style>
---
+++ src/components/player/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="player">
+    <div class="artist">
+      artist and song components
+    </div>
+    <div class="player">
+      main player setup
+    </div>
+    <div class="utility">
+      utility setup
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#player{
+  font-family: Arial, Helvetica, sans-serif;
+  color: aliceblue;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+  box-sizing: border-box;
+}
+</style>
